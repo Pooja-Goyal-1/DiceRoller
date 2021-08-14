cae3654267f32751b9c039c88f8f11fd978c8fa6
pooja <poojagoyal3072@gmail.com>
2021-08-14 13:22:04 +0530

    Initial commit

---
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -0,0 +1,44 @@
+package com.example.diceroller
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        // making the button interactive
+        val rollButton: Button = findViewById(R.id.button)
+        rollButton.setOnClickListener {
+            rollDice()
+        }
+        rollDice()
+    }
+// rolling the dice and printing the random output.
+    private fun rollDice() {
+        val dice = Dice(6)
+        val diceRoll = dice.roll()
+        val diceImage: ImageView = findViewById(R.id.imageView)
+
+        when(diceRoll) {
+            1 -> diceImage.setImageResource(R.drawable.dice_1)
+            2 -> diceImage.setImageResource(R.drawable.dice_2)
+            3 -> diceImage.setImageResource(R.drawable.dice_3)
+            4 -> diceImage.setImageResource(R.drawable.dice_4)
+            5 -> diceImage.setImageResource(R.drawable.dice_5)
+            6 -> diceImage.setImageResource(R.drawable.dice_6)
+
+        }
+
+    }
+}
+//creating dice class and calling random function.
+class Dice(val numSides: Int){
+    fun roll(): Int{
+        return (1..numSides).random()
+    }
+}
